126e444430878dc764283fd5cdfe7e987402e9a1
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-16 02:14:00 +0000

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,9 @@
-
+//tests for equality between two arrays
 const eqArrays = function(arrOne, arrTwo) {
-  return Array.isArray(arrOne) &&
-    Array.isArray(arrTwo) &&
-    arrOne.length === arrTwo.length &&
-    arrOne.every((val, index) => val === arrTwo[index]);
+  return Array.isArray(arrOne) &&   //determines whether the arrOne is an Array
+    Array.isArray(arrTwo) &&        //determines whether the arrTwo is an Array
+    arrOne.length === arrTwo.length &&  // checks if the number of elements in arrOne are equal to number of elements in arrayTwo
+    arrOne.every((val, index) => val === arrTwo[index]);  //tests whether all elements in arrOne and arratTwo are same
 
 };
 
