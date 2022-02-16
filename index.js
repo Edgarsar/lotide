b215d11409c19113687b655b32aff4aea4f088a8
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-16 21:34:49 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const head = require("./head");
+
+const tail = require("./tail");
+
+const middle = require("./middle");
+
+const without = require("./without");
+
+const flatten = require("./flatten");
+
+const countOnly = require("./countOnly");
+
+const countLetters = require("./countLetters");
+
+module.exports = {
+
+  head,
+  tail,
+  middle,
+  without,
+  flatten,
+  countOnly,
+  countLetters
+};
+
+
