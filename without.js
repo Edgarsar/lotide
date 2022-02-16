8c1610ff610d09a80735e826c9a34af848b3aaa5
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-16 19:49:29 +0000

    refactor without

--- without.js
+++ without.js
@@ -1,30 +1,12 @@
 // function that return a subset of a given array, removing unwanted elements
 const without = function(source, itemsToRemove) {
-  return source.filter(function(index) {
-    return itemsToRemove.indexOf(index) < 0;
-
-
-  });
-
-};
-// test case to make sure that the original array is not modified
-const eqArrays = function(arrOne, arrTwo) {
-  return Array.isArray(arrOne) &&
-    Array.isArray(arrTwo) &&
-    arrOne.length === arrTwo.length &&
-    arrOne.every((val, index) => val === arrTwo[index]);
-
-};
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅✅✅Assertion Passed:`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed:`);
-  }
+  // filter the source array
+  // if the item does not present in the itemsToRemove array then return -1
+  return source.filter(item =>
+    itemsToRemove.indexOf(item) < 0);
 
 };
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+module.exports = without;
 
