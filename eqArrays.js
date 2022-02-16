0ba2b1326adf211c53840002389687638be29654
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-16 02:20:12 +0000

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,7 +3,7 @@ const eqArrays = function(arrOne, arrTwo) {
   return Array.isArray(arrOne) &&   //determines whether the arrOne is an Array
     Array.isArray(arrTwo) &&        //determines whether the arrTwo is an Array
     arrOne.length === arrTwo.length &&  // checks if the number of elements in arrOne are equal to the number of elements in arrayTwo
-    arrOne.every((val, index) => val === arrTwo[index]);  //tests whether all elements in arrOne and arratTwo are the same
+    arrOne.every((val, index) => val === arrTwo[index]);  //checks if all elements in the arrOne and arrayTwo are identical
 
 };
 
