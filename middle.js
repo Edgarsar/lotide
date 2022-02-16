c8b0be29fd0c03a053a7e9c0dfb2d57ab42c37b2
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-16 16:36:23 +0000

    refactor middle

--- middle.js
+++ middle.js
@@ -1,8 +1,10 @@
 // return the middle-most element(s)
 const middle = function(array) {
+
   let result = [];
-  const midIndex = Math.floor(array.length / 2);  // finds largest integer less than or equal to the middle index 
-  console.log(midIndex)
+
+  const midIndex = Math.floor(array.length / 2);// finds largest integer less than or equal to the middle index
+
   if (array.length <= 2) {  // if an array has less then two elements then return empty array(no miidle)
     return result;
   }
@@ -12,6 +14,10 @@ const middle = function(array) {
   } else {
     result.push(array[midIndex - 1], array[midIndex]); //otherwise push the previous and the midile index elements
     return result;
-  };
+  }
+};
 
-  module.exports = middle;
+
+
+
+module.exports = middle;
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,9 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require("chai").assert
 
 const head = require("../head");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
 
