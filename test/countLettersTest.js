c2a03b8f7ac42c86e94976059604865a693d96b8
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-17 16:24:03 +0000

    add countLettersTest

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -6,15 +6,13 @@ describe("#countLetters", () => {
 
   it("returns '{ e: 1, d: 1, g: 1, a: 1, r: 1 }' for an input of 'edgar'", () => {
 
-    assert.deepEqual(countLetters("edgar"), { e: 1, d: 1, g: 1, a: 1, r: 1 })
+    assert.deepEqual(countLetters("edgar"), { e: 1, d: 1, g: 1, a: 1, r: 1 });
 
   });
 
   it ("returns '{ h: 1, e: 1, l: 2, o: 1}' for 'hello' ",()=>{
 
-    assert.deepEqual(countLetters("hello"),{ h: 1, e: 1, l: 2, o: 1})
-  })
+    assert.deepEqual(countLetters("hello"),{ h: 1, e: 1, l: 2, o: 1});
+  });
 
-
-
-})
+});
