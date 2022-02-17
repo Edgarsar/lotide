e138ec5530f48e5f5da493d6fe159b5e10cb8538
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-17 00:51:53 +0000

    refactor letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,14 @@
-const eqArrays = function(arrOne, arrTwo) {
-  return Array.isArray(arrOne) &&
-    Array.isArray(arrTwo) &&
-    arrOne.length === arrTwo.length &&
-    arrOne.every((val, index) => val === arrTwo[index]);
 
-};
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅✅✅Assertion Passed:`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed:`);
-  }
-
-};
-
-
+const assertArraysEqual = require("./assertArraysEqual");
 
 const letterPositions = function(sentence) {
+  //define a new empty object
   const results = {};
+  // remove white spaces
   const newStrings = sentence.replace(/ /g, "");
+
   for (let i = 0; i < newStrings.length; i++) {
+
     let el = newStrings[i];
 
     //if it is not true, that is the element is not there in the result
@@ -35,3 +24,6 @@ const letterPositions = function(sentence) {
 
 assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello").l, [2, 3]);
+
+
+module.exports = letterPositions;
