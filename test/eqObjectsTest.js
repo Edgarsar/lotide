f9277dafe6902a3c06fac3ef5af774d37ffd71ae
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-17 15:58:24 +0000

    add eqObjectsTest

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,17 @@
+const eqObjects = require('../eqObjects');
+
+const assert = require("chai").assert
+
+describe ("#eqObjects",()=>{
+it ("returns true for an input of '{c:1 , d: ['2' ,3]'and '{ d: ['2', 3], c: '1' }'",()=>{
+  const cd = { c: "1", d: ["2", 3] };
+  const dc = { d: ["2", 3], c: "1" };
+  assert.strictEqual(eqObjects(cd, dc), true);
+});
+it ("returns false for an input of '{c:1 , d: ['2' ,3]' and '{ c: '1', d: ['2', 3, 4] }'",()=>{
+const cd = { c: "1", d: ["2", 3] };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assert.strictEqual(eqObjects(cd, cd2), false);
+})
+
+})
