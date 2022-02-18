04e0e7cc1c9a6fe8e565c3df5d10cc29c6740756
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:05:26 +0000

    refactor without

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-// function that return a subset of a given array, removing unwanted elements
+// Takes in a source array and an itemsToRemove arrays and returns only the items from the source array that are not in the itemsToRemove array.
 const without = function(source, itemsToRemove) {
   // filter the source array
   // if the item does not present in the itemsToRemove array then return -1
