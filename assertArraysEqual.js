20b6690bb797a777121d0dc415b6d45f5fefbeb7
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:28:40 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,5 @@
-
 const eqArrays = require("./eqArrays");
-
-
+// Takes in 2 arrays and returns true if they are equal and false otherwise.
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   //returns a string representation of an object
   const inspect = require('util').inspect;
