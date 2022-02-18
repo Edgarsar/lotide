b76b7bd915de57a15085f7ccef240906012b3ddb
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:27:42 +0000

    refactor assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// tests for equality between the actual and the expected parameters
+// Takes in 2 values and returns true if they are equal and false otherwise 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
