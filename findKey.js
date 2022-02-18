91c86b1c33c69f67b104c7b145ed94a921b3c92e
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:16:58 +0000

    refactor findKey

--- findKey.js
+++ findKey.js
@@ -1,8 +1,4 @@
-const assertEqual = require("./assertEqual");
-
-// function takes in an object and a callback
-// it should scan the object and return the first key for which the callback returns a truthy value
-// if no key is found, then it should return undefined
+// Takes in an object and callback and returns the first key that meets the criteria specified in callback.
 const findKey = (object, callback) => {
   //returns an array of a given object's property names
   const Keys = Object.keys(object);
@@ -19,25 +15,3 @@ const findKey = (object, callback) => {
 
 module.exports = findKey;
 
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 2);
-
-const result2 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 3);
-
-
-
-assertEqual(result1, "noma");
-assertEqual(result2, "Akaleri");
