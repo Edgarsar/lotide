061b47752b59311f1079041ceba1f881f664bf51
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:10:17 +0000

    refactor takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -20,14 +20,3 @@ module.exports = takeUntil;
 
 
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
