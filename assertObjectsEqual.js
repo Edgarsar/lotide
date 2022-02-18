7d5f208dda0d64191b9025b610e502bdf5cebbb6
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-18 03:29:44 +0000

    refactor assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,7 @@
 const eqObjects = require("./eqObjects");
 // Takes in 2 objects and returns true if they are equal and false otherwise.
 const assertObjectsEqual = function(actual, expected) {
-
+  //returns a string representation of an object
   const inspect = require('util').inspect;
 
   if (eqObjects(actual, expected)) {
