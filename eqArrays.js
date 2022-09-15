135c5cb48e14b0e139762364bf5cb4e9e84bfa44
Edgarsar <edgarsargsyan@hotmail.com>
2022-09-15 23:44:26 +0000

    Implement solution for checking  a nested arrays equality

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,14 @@
 // Takes in 2 arrays and returns true if the arrays are equal and false otherwise.
-const eqArrays = function(arrOne, arrTwo) {
+const eqArrays = function (arrOne, arrTwo) {
+  for (let i = 0; i < arrOne.length; i++) {
+    for (let j = 0; j < arrTwo.length; j++) {
+      if (Array.isArray(arrOne[i]) && Array.isArray(arrTwo[j])) {
+        return eqArrays(arrOne[i], arrTwo[j])
+      }
+
+    }
+  }
+
   return Array.isArray(arrOne) &&   //determines whether the arrOne is an Array
     Array.isArray(arrTwo) &&        //determines whether the arrTwo is an Array
     arrOne.length === arrTwo.length &&  // checks if the number of elements in arrOne are equal to the number of elements in arrayTwo
